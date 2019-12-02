1d1130388678cc7b301b0af4c77b2499ae23b71c
ZVNexus <zvnexus@outlook.com>
2019-12-02 15:39:33 -0500

    Update fingerprint to QQ1A.191205.011.
    Change-Id: I20a075eec01116ed5212579e0b56c9dd66e46f58

--- du.mk
+++ du.mk
@@ -31,12 +31,12 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="sargo" \
     TARGET_DEVICE="sargo" \
-    PRIVATE_BUILD_DESC="sargo-user 10 QP1A.191105.003 5899767 release-keys"
+    PRIVATE_BUILD_DESC="sargo-user 10 QQ1A.191205.011 6009058 release-keys"
 
-BUILD_FINGERPRINT := "google/sargo/sargo:10/QP1A.191105.003/5899767:user/release-keys"
+BUILD_FINGERPRINT := "google/sargo/sargo:10/QQ1A.191205.011/6009058:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/sargo/sargo:10/QP1A.191105.003/5899767:user/release-keys
+    ro.build.fingerprint=google/sargo/sargo:10/QQ1A.191205.011/6009058:user/release-keys
 
 BOARD_PREBUILT_VENDORIMAGE := vendor/images/sargo/vendor.img
 
